7ad794a06e37ccfc430379e0f7adea44ed178593
MarthoxGJ <43354689+MarthoxGJ@users.noreply.github.com>
2024-11-26 17:18:54 -0500

    First setup of the website

---
+++ src/i18n/ui.ts
@@ -0,0 +1,43 @@
+export const languages = {
+    en: "English",
+    es: "Español",
+    fr: "Français",
+};
+
+export const defaultLang = "en";
+
+export const ui = {
+    en: {
+        "nav.home": "Home",
+        "nav.about": "About Me",
+        "nav.blog": "Blog",
+        "nav.community": "Community",
+        "nav.projects": "Projects",
+        "inner_nav.summary": "Summary",
+        "inner_nav.music": "Music",
+        "inner_nav.history": "History",
+        "inner_nav.curriculum": "Curriculum",
+    },
+    es: {
+        "nav.home": "Inicio",
+        "nav.about": "Sobre Mí",
+        "nav.blog": "Blog",
+        "nav.community": "Comunidad",
+        "nav.projects": "Proyectos",
+        "inner_nav.summary": "Resumen",
+        "inner_nav.music": "Música",
+        "inner_nav.history": "Historia",
+        "inner_nav.curriculum": "Currículum",
+    },
+    fr: {
+        "nav.home": "Inicio",
+        "nav.about": "Sobre Mí",
+        "nav.blog": "Blog",
+        "nav.community": "Comunidad",
+        "nav.projects": "Proyectos",
+        "inner_nav.summary": "Resumen",
+        "inner_nav.music": "Música",
+        "inner_nav.history": "Historia",
+        "inner_nav.curriculum": "Currículum",
+    },
+} as const;
