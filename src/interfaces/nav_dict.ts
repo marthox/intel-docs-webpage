859548013858430804d98e6119f07f59eb62d596
Yion <105884704+Yi0nn@users.noreply.github.com>
2024-11-27 00:35:35 -0500

    Navbar 0.1

---
+++ src/interfaces/nav_dict.ts
@@ -0,0 +1,6 @@
+export interface Navbar {
+    solutions: { text: string; link: string }[];
+    products: { text: string; link: string }[];
+    resources: { text: string; link: string }[];
+    company: { text: string; link: string }[];
+}
