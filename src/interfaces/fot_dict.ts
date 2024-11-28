29096c7b228728d258a77df093dcb5cc627a6dfb
Yion <105884704+Yi0nn@users.noreply.github.com>
2024-11-28 02:36:47 -0500

    Update Footer

---
+++ src/interfaces/fot_dict.ts
@@ -0,0 +1,7 @@
+export interface Footer {
+    explore: { text: string; link: string }[];
+    partner: { text: string; link: string }[];
+    legal: { text: string; link: string }[];
+    contact: { text: string; link: string }[];
+}
+
