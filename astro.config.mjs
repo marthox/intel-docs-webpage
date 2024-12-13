5b8597203d73bf178c3c73f12ab5b2e4197a79d7
MarthoxGJ <43354689+MarthoxGJ@users.noreply.github.com>
2024-12-13 10:21:05 -0500

    Add deploy config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,18 @@
 import { defineConfig } from "astro/config";
 
 import react from "@astrojs/react";
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-  i18n: {
-      defaultLocale: "en",
-      locales: ["fr", "en"],
-      routing: {
-          prefixDefaultLocale: true,
-      },
-  },
-
-  integrations: [react()],
+    i18n: {
+        defaultLocale: "en",
+        locales: ["fr", "en"],
+        routing: {
+            prefixDefaultLocale: true,
+        },
+    },
+    output: "server",
+    adapter: netlify(),
+    integrations: [react()],
 });
